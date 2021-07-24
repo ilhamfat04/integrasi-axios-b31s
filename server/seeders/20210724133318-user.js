33768bc8d0dfa506ea3ca769a360d25e8a781c3f
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2021-07-24 20:45:02 +0700

    insert data with axios

---
+++ server/seeders/20210724133318-user.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          email: "admin",
+          password:
+            "$2b$10$nUSAIUyuUbbl3zvRrFH/Te.qszxuRt59C65OIkQPcyvH6NFxDrUeS", //123456
+          name: "admin",
+          status: "admin",
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
